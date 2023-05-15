9ef2a3cf65860b3adde571c344bdf83671880b13
Veronique Doss <vdoss@student.21-school.ru>
2023-05-15 13:05:53 +0700

    Docker compose launched

--- index.js
+++ index.js
@@ -3,36 +3,21 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv';
 
-import router from './src/routes/index.js'
-
 if (process.env.NODE_ENV !== 'production') {
     dotenv.config();
 }
 
-const {
-    MONGO_USERNAME,
-    MONGO_PASSWORD,
-    MONGO_HOSTNAME,
-    MONGO_PORT,
-    MONGO_DB, 
-    MONGO_URI
-} = process.env;
+import router from './src/routes/index.js'
 
-// const DB_URL = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
-
-console.log({
-    MONGO_USERNAME,
-    MONGO_PASSWORD,
-    MONGO_HOSTNAME,
-    MONGO_PORT,
-    MONGO_DB
-}, DB_URL);
-// const DB_URL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
+import { url } from './config/db.js';
 
 const options = {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    // connectTimeoutMS: 10000,
+    directConnection: true
 };
+
+const APP_PORT = process.env.NODE_DOCKER_PORT || 8080;
 
 const app = express()
 
@@ -47,19 +32,15 @@ app.use('/api', router)
 async function start() {
     try {
         console.log("MongoDB try to be connected");
-        await mongoose.connect(MONGO_URI, options).then(
+        await mongoose.connect(url, options).then(
             () => console.log("MongoDB is connected")
         );
-        app.listen(PORT, () => {
-            console.log(`Server started on PORT ${PORT}`)
+        app.listen(APP_PORT, () => {
+            console.log(`Server started on PORT ${APP_PORT}`)
         });
     } catch (e) {
-        console.log("MongoDB not connected");
-        
         console.error(e);
     } 
 }
 
 start();
-
-
